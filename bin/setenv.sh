060860e320fc7052cf0628b311f1c3f7a6f0b224
Stefan Eletzhofer <se@nexiles.de>
2012-10-18 14:47:13 +0200

    fix jspack

--- bin/setenv.sh
+++ bin/setenv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # environment for nexiles.companion build
-BASEDIR="~/develop/nexiles/tools"
+BASEDIR=$(dirname $0)/..
 SHRINKSAFE="$BASEDIR/shrinksafe/shrinksafe.jar"
 JSPACK="$BASEDIR/jspack"
