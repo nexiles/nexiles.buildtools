7423c7699249ee993104ba841cc1365c8dd2a98b
Stefan Eletzhofer <se@nexiles.de>
2013-06-24 11:19:27 +0200

    gateway reload command
    Update the gw reload fab command to include a path.

--- fab/gateway.py
+++ fab/gateway.py
@@ -16,11 +16,14 @@ from fabric.colors import green, yellow, red
 from fabric.contrib.console import confirm
 
 @task
-def reload():
+def reload(path=None):
     """reload -- trigger a nexiles|gateway servlet reload."""
+    if path is None:
+        path = "version"
     host = local('grep "^java.rmi.server.hostname" $WT_HOME/codebase/wt.properties | cut -d = -f2', capture=True)
     user = os.environ.get("WTUSER", "orgadmin")
     pw   = os.environ.get("WTPASS", "orgadmin")
-    cmd  = "curl -i --user {user}:{pw} -X GET -H 'nexiles-gateway-reload: true' http://{host}/Windchill/servlet/nexiles/tools/version".format(user=user, pw=pw, host=host)
+    cmd  = "curl -i --user {user}:{pw} -X GET -H 'nexiles-gateway-reload: true' http://{host}/Windchill/servlet/nexiles/tools/{path}".format(user=user, pw=pw, host=host, path=path)
+    print red("RELOADING: " + path)
     local(cmd)
 
